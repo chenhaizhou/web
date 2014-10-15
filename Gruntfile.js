f04fe8de7e22f81a00c6984528b44bf1eaf86ab0
chenhaizhou <hzchen@CNhzchen.local>
2014-10-15 11:06:14 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,19 @@ module.exports = function(grunt) {
 	          document: true
 	        }
 	      }
-	    }
+	    },
+	    concat: {
+		  options: {
+		    // 定义一个用于插入合并输出文件之间的字符
+		    separator: ';'
+		  },
+		  dist: {
+		    // 将要被合并的文件
+		    src: ['assets/scripts/**/*.js'],
+		    // 合并后的JS文件的存放位置
+		    dest: 'assets/<%= pkg.name %>.min.js'
+		  }
+		}
 		
 	});
 
