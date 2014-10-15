410346387ba4c5d3db7adf03bd68cc27b572e5e1
chenhaizhou <hzchen@CNhzchen.local>
2014-10-15 10:49:59 +0800

    update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,21 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+	      files: ['Gruntfile.js', 'assets/scripts/**/*.js', 'spec/**/*.js'],
+	      options: {
+	        //这里是覆盖JSHint默认配置的选项
+	        globals: {
+	          jQuery: true,
+	          console: true,
+	          module: true,
+	          document: true
+	        }
+	      }
+	    }
 		
-	})
+	});
+
+	grunt.registerTask('default', ['jshint']);
+
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,7 +2,7 @@ define(function(){
 	require.config({
 		paths: {
 			"jquery": "bower_components/jquery/dist/jquery.min.js",
-			"angular" : "bower_components/angular/angular.min.js",
+			"angular" : "bower_components/angular/angular.min.js"
 		}
 	});
 });
