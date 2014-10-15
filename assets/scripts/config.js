5d37d90b79b1bc5b2541ec6d9183e97123b03152
chenhaizhou <hzchen@CNhzchen.local>
2014-10-15 11:48:55 +0800

    add config

---
+++ assets/scripts/config.js
@@ -0,0 +1,6 @@
+require.config({
+	paths: {
+		"jquery": "bower_components/jquery/dist/jquery.min.js",
+		"angular" : "bower_components/angular/angular.min.js"
+	}
+});
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,3 @@
-define(function(){
-	require.config({
-		paths: {
-			"jquery": "bower_components/jquery/dist/jquery.min.js",
-			"angular" : "bower_components/angular/angular.min.js"
-		}
-	});
+define(["config",t1],function(){
+	console.log(2);
 });
