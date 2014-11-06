f9c9281970de712a30a09d459b6b92e6c7e20af2
chenhaizhou <hzchen@CNhzchen.local>
2014-11-06 16:30:29 +0800

    add uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,11 +51,7 @@ module.exports = function(grunt) {
 		},
 		concat: {
 			js: {
-				options: {
-				    // 定义一个用于插入合并输出文件之间的字符
-				    separator: ';'
-				},
-				dist: {
+				
 				    // 将要被合并的文件
 				    src: [
 				    	'bower_components/jquery/dist/jquery.js',
@@ -64,8 +60,8 @@ module.exports = function(grunt) {
 				    	'app/src/scripts/**/*.js'
 			    	],
 				    // 合并后的JS文件的存放位置
-				    dest: 'app/dist/js/<%= pkg.name %>.min.js'
-				}
+				    dest: 'app/dist/js/<%= pkg.name %>.js'
+				
 			},
 			css: {
 				src: ['bower_components/bootstrap/dist/css/bootstrap.css', 'app/src/css/*.css'],
@@ -83,6 +79,19 @@ module.exports = function(grunt) {
 			}
 
 		},
+		uglify: {
+		    options: {
+		      mangle: {
+		        except: ['jQuery']
+		      }
+		    },
+		    dist: {
+		      files: {
+		        'app/dist/js/<%= pkg.name %>.min.js': ['app/dist/js/<%= pkg.name %>.js']
+		      }
+		    }
+		  },
+		clean: ['app/dist'],
 		watch: {
 			sass: {
 				files: 'app/src/sass/*.scss',
@@ -93,14 +102,16 @@ module.exports = function(grunt) {
 
 	});
 	
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	//grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-karma');
-	grunt.loadNpmTasks('karma-chrome-launcher');
+	//grunt.loadNpmTasks('karma-chrome-launcher');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	
@@ -109,7 +120,9 @@ module.exports = function(grunt) {
 	grunt.registerTask('css', ['sass']);
 	//grunt.registerTask('compass', ['compass']);
 
+	grunt.registerTask('js', ['jshint','concat:js','uglify']);
 
+	grunt.registerTask('cls', ['clean']);
 
 	grunt.registerTask('default', ['jshint','copy','css','concat:css','cssmin']);
 
