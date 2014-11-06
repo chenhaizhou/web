60bdafb650a686112936ca6ea177f1ae80b3753d
chenhaizhou <hzchen@CNhzchen.local>
2014-11-06 15:50:46 +0800

    ad test coverage

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,16 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    //reporters: ['progress'],
+    reporters: ['progress','coverage'],
+    preprocessors : {
+        'app/src/scripts/**/*.js': ['coverage']
+    },
+    coverageReporter: {
+        type : 'html',
+        dir : 'coverage/'
+    },
+
 
 
     // web server port
